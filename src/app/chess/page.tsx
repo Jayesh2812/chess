952bbe9119ef95c9042541ec7b7d1f0933f88918
Jayesh Deorukhkar <jddeorukhkar@gmail.com>
2023-12-25 23:24:24 +0530

    feat: initial commit

---
+++ src/app/chess/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Square from "@/components/Square.component";
+
+function Chess() {
+  return <Square></Square>;
+}
+
+export default Chess;
---
+++ src/components/Square.component.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface ISquareProps {
+  rank: number;
+  file: string;
+}
+
+function Square({ file, rank }: ISquareProps) {
+  return <div></div>;
+}
+
+export default Square;
